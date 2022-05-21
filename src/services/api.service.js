5bf0bc619d6219a282ec1759fbbaf18aebbd2dcd
nghia2knt <nghiazexal@gmail.com>
2022-05-21 09:41:30 +0700

    commit 3

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const moment = require('moment')
 
-const baseApi = "http://3.80.122.27:9002/api/";
+const baseApi = "https://4monce5w19.execute-api.us-east-2.amazonaws.com/api/";
 
 const apiService = {
   auth(url = baseApi + "auth/") {
